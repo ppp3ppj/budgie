edb73bf6f87f58d803949b041de358a425156891
ppp3ppj <ppp.3ppj@gmail.com>
2025-03-24 21:05:16 +0700

    add: testing fixtures and optimize test use tracking fixtures

--- test/budgie/tracking_test.exs
+++ test/budgie/tracking_test.exs
@@ -2,19 +2,15 @@ defmodule Budgie.TrackingTest do
   use Budgie.DataCase
   alias Budgie.Tracking
 
+  import Budgie.TrackingFixtures
+
   describe "budgets" do
     alias Budgie.Tracking.Budget
 
     test "create_budget/2 with valid data creates budget" do
       user = Budgie.AccountsFixtures.user_fixture()
 
-      valid_attrs = %{
-        name: "some name",
-        description: "some description",
-        start_date: ~D[2025-01-01],
-        end_date: ~D[2025-01-31],
-        creator_id: user.id
-      }
+      valid_attrs = valid_budget_attributes(%{creator_id: user.id})
 
       assert {:ok, %Budget{} = budget} = Tracking.create_budget(valid_attrs)
       assert budget.name == "some name"
@@ -25,37 +21,29 @@ defmodule Budgie.TrackingTest do
     end
 
     test "create_budget/2 with requires name" do
-      user = Budgie.AccountsFixtures.user_fixture()
+      attrs =
+        valid_budget_attributes()
+        |> Map.delete(:name)
 
-      attrs_without_name = %{
-        description: "some description",
-        start_date: ~D[2025-01-01],
-        end_date: ~D[2025-01-31],
-        creator_id: user.id
-      }
-
-      assert {:error, %Ecto.Changeset{} = changeset} = Tracking.create_budget(attrs_without_name)
+      assert {:error, %Ecto.Changeset{} = changeset} = Tracking.create_budget(attrs)
 
       assert changeset.valid? == false
       assert %{name: ["can't be blank"]} = errors_on(changeset)
     end
 
     test "create_budget/2 with requires valid dates" do
-      user = Budgie.AccountsFixtures.user_fixture()
+      attrs =
+        valid_budget_attributes()
+        |> Map.merge(%{
+          start_date: ~D[2025-01-31],
+          end_date: ~D[2025-01-01]
+        })
 
-      attrs_without_name = %{
-        name: "some name",
-        description: "some description",
-        start_date: ~D[2025-01-31],
-        end_date: ~D[2025-01-01],
-        creator_id: user.id
-      }
-
-      assert {:error, %Ecto.Changeset{} = changeset} = Tracking.create_budget(attrs_without_name)
+      assert {:error, %Ecto.Changeset{} = changeset} = Tracking.create_budget(attrs)
       assert %{end_date: ["must end after start date"]} = errors_on(changeset)
 
       assert changeset.valid? == false
-      #dbg(changeset)
+      # dbg(changeset)
     end
   end
 end
