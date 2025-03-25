660249deb93f23448e37c8f6b0310291e03b2ed0
ppp3ppj <ppp.3ppj@gmail.com>
2025-03-25 20:00:39 +0700

    add: testing the liveview

---
+++ test/budgie_web/live/budget_list_live_test.exs
@@ -0,0 +1,26 @@
+defmodule BudgieWeb.Live.BudgetListLiveTest do
+  use BudgieWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  import Budgie.TrackingFixtures
+
+  setup do
+    user = Budgie.AccountsFixtures.user_fixture()
+
+    %{user: user}
+  end
+
+  describe "Index view" do
+    test "show budget when one exists", %{conn: conn, user: user} do
+      budget = budget_fixture(%{creator_id: user.id})
+
+      conn = log_in_user(conn, user)
+      {:ok, lv, html} = live(conn, ~p"/budgets")
+
+      open_browser(lv)
+
+      assert html =~ budget.name
+      assert html =~ budget.description
+    end
+  end
+end
