89956d80ba23eaca7ab183cbdf861ae9f0c2c1a8
ppp3ppj <ppp.3ppj@gmail.com>
2025-03-27 20:45:49 +0700

    add: test show budget and error

---
+++ test/budgie_web/live/budget_show_live_test.exs
@@ -0,0 +1,35 @@
+defmodule BudgieWeb.Live.BudgetShowLiveTest do
+  use BudgieWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  import Budgie.TrackingFixtures
+
+  setup do
+    user = Budgie.AccountsFixtures.user_fixture()
+    budget = budget_fixture(%{creator_id: user.id})
+
+    %{user: user, budget: budget}
+  end
+
+  describe "Show budget" do
+    test "show budget when it exists", %{conn: conn, user: user, budget: budget} do
+      conn = log_in_user(conn, user)
+      {:ok, _lv, html} = live(conn, ~p"/budgets/#{budget}")
+
+      assert html =~ budget.name
+    end
+
+    test "redirects to budget list page when budget does not exit", %{conn: conn, user: user} do
+      fake_budget_id = Ecto.UUID.generate()
+
+      conn = log_in_user(conn, user)
+
+      {:ok, conn} =
+        live(conn, ~p"/budgets/#{fake_budget_id}")
+        |> follow_redirect(conn, ~p"/budgets")
+
+      assert %{"error" => "Budget not found"} = conn.assigns.flash
+    end
+  end
+end
+
