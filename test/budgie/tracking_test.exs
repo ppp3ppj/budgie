2f1af26f02a06b86c68c0e1e7ec07fe07cd2bf8b
ppp3ppj <ppp.3ppj@gmail.com>
2025-04-08 20:00:27 +0700

    feat: add more test for budget tarnsaction maximum transaction amount

--- test/budgie/tracking_test.exs
+++ test/budgie/tracking_test.exs
@@ -1,4 +1,5 @@
 defmodule Budgie.TrackingTest do
+  alias Budgie.Tracking.Budget
   use Budgie.DataCase
   alias Budgie.Tracking
 
@@ -31,6 +32,7 @@ defmodule Budgie.TrackingTest do
       attrs =
         params_with_assocs(:budget)
         |> Map.delete(:name)
+
       assert {:error, %Ecto.Changeset{} = changeset} = Tracking.create_budget(attrs)
 
       assert changeset.valid? == false
@@ -66,6 +68,90 @@ defmodule Budgie.TrackingTest do
     test "get_budget/1 returns nil when budget doesn't exist" do
       _unrelated_budget = insert(:budget)
       assert is_nil(Tracking.get_budget("74984201-e309-4408-b731-197c6ee5eeb1"))
+    end
+  end
+
+  describe "budget transactions" do
+    alias Budgie.Tracking.BudgetTransaction
+
+    @invalid_attrs %{description: nil, effective_date: nil, amount: nil, creator_id: nil}
+
+    test "list_transaction/1 returns all transactions in the budget, by id" do
+      budget = insert(:budget)
+
+      transactions = insert_pair(:budget_transaction, budget: budget)
+      _other_transactions = insert_pair(:budget_transaction)
+
+      assert Tracking.list_transactions(budget.id) == without_preloads(transactions)
+    end
+
+    test "list_transaction/1 return all transactions in the budget, by reference" do
+      budget = insert(:budget)
+
+      transactions = insert_pair(:budget_transaction, budget: budget)
+      _other_transactions = insert_pair(:budget_transaction)
+
+      assert Tracking.list_transactions(budget) == without_preloads(transactions)
+    end
+
+    test "list_transactions/2 returns transactions with preloads" do
+      budget = insert(:budget)
+
+      transactions = insert_pair(:budget_transaction, budget: budget)
+
+      assert Tracking.list_transactions(budget, preload: [budget: :creator]) == transactions
+    end
+
+    test "list_transaction/2 returns sorted transactions" do
+      budget = insert(:budget)
+
+      late_transaction =
+        insert(:budget_transaction, budget: budget, effective_date: ~D[2025-12-31])
+
+      early_transaction =
+        insert(:budget_transaction, budget: budget, effective_date: ~D[2025-01-01])
+
+      mid_transaction =
+        insert(:budget_transaction, budget: budget, effective_date: ~D[2025-06-15])
+
+      expected_transactions =
+        [
+          late_transaction,
+          mid_transaction,
+          early_transaction
+        ]
+        |> without_preloads()
+
+      assert Tracking.list_transactions(budget, order_by: [desc: :effective_date]) ==
+               expected_transactions
+    end
+
+    test "create_transaction/1 with valid data creates a transaction" do
+      budget = insert(:budget)
+
+      valid_params = params_with_assocs(:budget_transaction, budget: budget)
+
+      assert {:ok, %BudgetTransaction{} = transaction} = Tracking.create_transaction(valid_params)
+
+      assert transaction.type == valid_params.type
+      assert transaction.description == valid_params.description
+      assert transaction.effective_date == valid_params.effective_date
+      assert transaction.amount == valid_params.amount
+      assert transaction.budget_id == budget.id
+    end
+
+    test "change_transaction/1 with invalid data returns an valid changeset" do
+      assert %Ecto.Changeset{valid?: false} =
+               Tracking.change_transaction(%BudgetTransaction{}, @invalid_attrs)
+    end
+
+    test "change_transaction/1 with HUGE amount returns an error" do
+      params =
+        params_with_assocs(:budget_transaction)
+        |> Map.put(:amount, Decimal.new("9999999"))
+
+      assert %Ecto.Changeset{valid?: false} =
+               Tracking.change_transaction(%BudgetTransaction{}, params)
     end
   end
 end
