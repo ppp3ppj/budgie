96723a36189a2e9772499b29c3be980739b27bab
ppp3ppj <ppp.3ppj@gmail.com>
2025-04-20 20:46:38 +0700

    add: test for update transaction

--- test/budgie_web/live/budget_show_live_test.exs
+++ test/budgie_web/live/budget_show_live_test.exs
@@ -1,9 +1,10 @@
 defmodule BudgieWeb.Live.BudgetShowLiveTest do
   use BudgieWeb.ConnCase, async: true
 
-  import Phoenix.LiveViewTest
-  alias Budget.Repo
+  alias Budgie.Repo
   alias Budgie.Tracking.BudgetTransaction
+
+  import Phoenix.LiveViewTest
 
   setup do
     budget = insert(:budget)
@@ -54,7 +55,8 @@ defmodule BudgieWeb.Live.BudgetShowLiveTest do
 
       assert has_element?(lv, "#transaction-modal")
     end
- test "creates a transaction", %{
+
+    test "creates a transaction", %{
       conn: conn,
       user: user,
       budget: budget
@@ -116,7 +118,107 @@ defmodule BudgieWeb.Live.BudgetShowLiveTest do
 
       assert html =~ "must be greater than 0"
     end
+  end
 
+  describe "Edit transaction modal" do
+    setup %{budget: budget} do
+      transaction = insert(:budget_transaction, budget: budget)
 
+      %{transaction: transaction}
+    end
+
+    test "shows edit transaction modal", %{
+      conn: conn,
+      user: user,
+      budget: budget,
+      transaction: transaction
+    } do
+      conn = log_in_user(conn, user)
+      {:ok, lv, _html} = live(conn, ~p"/budgets/#{budget}/transactions/#{transaction}/edit")
+
+      assert has_element?(lv, "#transaction-modal")
+
+      assert has_element?(
+               lv,
+               ~s|#transaction-modal input[name='transaction[description]'][value='#{transaction.description}']|
+             )
+    end
+
+    test "redirects on invalid transaction id", %{conn: conn, user: user, budget: budget} do
+      conn = log_in_user(conn, user)
+
+      {:ok, conn} =
+        live(conn, ~p"/budgets/#{budget}/transactions/invalid/edit")
+        |> follow_redirect(conn, ~p"/budgets/#{budget}")
+
+      assert %{"error" => "Transaction not found"} = conn.assigns.flash
+    end
+
+    test "validation errors are presented when form is changed with invalid input", %{
+      conn: conn,
+      user: user,
+      budget: budget,
+      transaction: transaction
+    } do
+      conn = log_in_user(conn, user)
+      {:ok, lv, _html} = live(conn, ~p"/budgets/#{budget}/transactions/#{transaction}/edit")
+
+      params = params_for(:budget_transaction, amount: Decimal.new("-42"))
+
+      form =
+        form(lv, "#transaction-modal form", %{
+          "transaction" => params
+        })
+
+      html = render_change(form)
+
+      assert html =~ "must be greater than 0"
+    end
+
+    test "validation errors are presented when form is submitted with invalid input", %{
+      conn: conn,
+      user: user,
+      budget: budget,
+      transaction: transaction
+    } do
+      conn = log_in_user(conn, user)
+      {:ok, lv, _html} = live(conn, ~p"/budgets/#{budget}/transactions/#{transaction}/edit")
+
+      params = params_for(:budget_transaction, amount: Decimal.new("-42"))
+
+      form =
+        form(lv, "#transaction-modal form", %{
+          "transaction" => params
+        })
+
+      html = render_submit(form)
+
+      assert html =~ "must be greater than 0"
+    end
+
+    test "updates transaction", %{
+      conn: conn,
+      user: user,
+      budget: budget,
+      transaction: transaction
+    } do
+      conn = log_in_user(conn, user)
+      {:ok, lv, _html} = live(conn, ~p"/budgets/#{budget}/transactions/#{transaction}/edit")
+
+      new_amount = (:rand.uniform(5000) / 100) |> Decimal.from_float()
+      params = params_for(:budget_transaction, amount: new_amount)
+
+      form =
+        form(lv, "#transaction-modal form", %{
+          "transaction" => params
+        })
+
+      {:ok, _lv, html} = render_submit(form) |> follow_redirect(conn)
+
+      assert html =~ "Transaction updated"
+
+      transaction = Repo.get(BudgetTransaction, transaction.id)
+      assert transaction.amount == new_amount
+    end
   end
 end
